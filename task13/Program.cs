8e29c0006a46704b79d6e250e40823cfb434b8d3
Aleksandra Altaeva <svvvlt0310@gmail.com>
2023-03-16 19:39:08 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+string num = Console.ReadLine()!;
+
+if (num.Length < 3)
+{
+    Console.Write($"В {num} третьей цифры нет.");
+}
+else
+{
+    Console.Write($"{num} -> {num[2]}");
+}
