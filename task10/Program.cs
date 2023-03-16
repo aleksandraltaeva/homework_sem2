9dcf769a78dc0ffd712d2d84f8ff4225f27973cd
Aleksandra Altaeva <svvvlt0310@gmail.com>
2023-03-16 19:24:39 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+string num = Console.ReadLine()!;
+
+if(num.Length == 3)
+{
+    Console.WriteLine($"{num} -> {num[1]}");
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число.");
+}
