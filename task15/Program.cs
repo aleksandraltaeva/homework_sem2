0d72b25528a62687c824a9a8c53065c949d875c2
Aleksandra Altaeva <svvvlt0310@gmail.com>
2023-03-16 19:33:05 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите номер дня недели: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num > 0 && num < 8)
+{
+    if (num == 6 || num == 7)
+    {
+     Console.Write($"{num} является выходным.");   
+    }
+    else
+    {
+        Console.Write($"{num} не является выходным."); 
+    }
+}
+else 
+{
+    Console.Write("Такого дня недели не существует.");  
+}
