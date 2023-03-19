c10e828d3b74b81b02c38e3ee98cef6637502b65
Aleksandra Altaeva <svvvlt0310@gmail.com>
2023-03-19 16:13:41 +0300

    добавили математический метод в task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,11 +1,30 @@
 ﻿Console.Write("Введите число: ");
-string num = Console.ReadLine()!;
+//строковый метод
+// string num = Console.ReadLine()!;
 
-if (num.Length < 3)
+// if (num.Length < 3)
+// {
+//     Console.Write($"В {num} третьей цифры нет.");
+// }
+// else
+// {
+//     Console.Write($"{num} -> {num[2]}");
+// }
+
+//математический метод
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num = num1;
+
+if (num >= 100)
 {
-    Console.Write($"В {num} третьей цифры нет.");
+    while (num > 999)
+    {
+        num /= 10;
+    }
+     Console.Write($"{num1} -> {num % 10}");
 }
 else
 {
-    Console.Write($"{num} -> {num[2]}");
+    Console.Write($"В {num1} третьей цифры нет.");
 }
