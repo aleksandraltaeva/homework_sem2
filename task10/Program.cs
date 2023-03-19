727fc26a1e92b0bc8a85dcaeb86fce0b78ccdbb1
Aleksandra Altaeva <svvvlt0310@gmail.com>
2023-03-19 15:47:12 +0300

    добавили математический метод в task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,9 +1,24 @@
 ﻿Console.Write("Введите трехзначное число: ");
-string num = Console.ReadLine()!;
 
-if(num.Length == 3)
+// строковый метод
+// string num = Console.ReadLine()!;
+
+// if(num.Length == 3)
+// {
+//     Console.WriteLine($"{num} -> {num[1]}");
+// }
+// else
+// {
+//     Console.WriteLine("Введено не трехзначное число.");
+// }
+
+// математический метод
+int num = Convert.ToInt32(Console.ReadLine());
+int second = 0;
+if (num >= 100 && num <= 999)
 {
-    Console.WriteLine($"{num} -> {num[1]}");
+    second = num / 10 % 10;
+    Console.WriteLine($"{num} -> {second}");
 }
 else
 {
